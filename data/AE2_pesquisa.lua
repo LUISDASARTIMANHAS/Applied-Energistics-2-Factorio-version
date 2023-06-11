8649bdf2a80de72ab40a9d9405c402b2e84b0121
LUIS_DAS_ARTIMANHAS <75493473+LUISDASARTIMANHAS@users.noreply.github.com>
2023-06-11 15:52:53 -0300

    Add files via upload

--- data/AE2_pesquisa.lua
+++ data/AE2_pesquisa.lua
@@ -1,17 +1,62 @@
-data:extend({{
+data:extend({ {
     type = "technology",
     name = "label:AE2",
-    prerequisites = {"plastics"},
-    effects = {    },
+    icon = "__Applied-Energistics-2__/graficos/technology/AE2.png",
+    icon_size = 128,
+    icon_mipmaps = 4,
+    prerequisites = {
+        "plastics"
+    },
+    effects =
+    {
+        -- {
+        --     type = "unlock-recipe",
+        --     recipe = "",
+        -- },
+        -- {
+        --     type = "unlock-recipe",
+        --     recipe = "",
+        -- },
+    },
     unit = {
         count = 150,
+        time = 30,
         ingredients = {
-            {"automation-science-pack", 1}
+            { "automation-science-pack", 500}
         },
-        time = 30
-    },
+        order = ""
+    }
+}
+})
 
-    icon = "__Applied-Energistics-2__/graficos/technology/AE2.png",
-    icon_size = 126,
-    icon_mipmaps = 4
-}})
+data:extend({ {
+    type = "technology",
+    name = "label:AE2-autocraft",
+    icon = "__Applied-Energistics-2__/graficos/technology/64k_crafting_storage.png",
+    icon_size = 128,
+    icon_mipmaps = 4,
+    prerequisites = {
+        "plastics",
+        "AE2"
+    },
+    effects =
+    {
+        -- {
+        --     type = "unlock-recipe",
+        --     recipe = "void-pipe",
+        -- },
+        -- {
+        --     type = "unlock-recipe",
+        --     recipe = "",
+        -- },
+    },
+    unit = {
+        count = 150,
+        time = 30,
+        ingredients = {
+            { "automation-science-pack", 500}
+        },
+        order = ""
+    }
+}
+})
