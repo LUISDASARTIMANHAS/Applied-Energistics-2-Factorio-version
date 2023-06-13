1c40a6060414d53b78bdf15319c583448479d454
LUIS_DAS_ARTIMANHAS <75493473+LUISDASARTIMANHAS@users.noreply.github.com>
2023-06-13 00:40:49 -0300

    Add files via upload

--- data.lua
+++ data.lua
@@ -4,6 +4,7 @@ require("data/pesquisa/AE2_pesquisa.lua")
 require("graficos/style.lua")
 require("data/advanced_press_recipe.lua")
 require("data/processors_recipe.lua")
+require("data/AE2_grupos.lua")
 
 -- itens craftaveis e colocaveis
 -- require("data/items/server_block.lua")
