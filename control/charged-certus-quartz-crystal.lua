faf706c7a99d84ce607e7a8e29aada786fbddcd1
LUIS DAS ARTIMANHAS <luisaugustodesouza785@gmail.com>
2023-07-21 14:41:20 -0300

    updated

--- control/charged-certus-quartz-crystal.lua
+++ control/charged-certus-quartz-crystal.lua
@@ -8,21 +8,21 @@ data:extend({
   {
     type = "resource",
     name = "charged-certus-quartz-crystal-resource",
-    icon = path_main.. "graficos/items/charged_certus_quartz_crystal.png", -- Adicione o ícone do minério
+    icon = path_main .. "graficos/items/charged_certus_quartz_crystal.png", -- Adicione o ícone do minério
     icon_size = 32,
-    flags = {"placeable-neutral"},
+    flags = { "placeable-neutral" },
     order = "a-b-a",
     infinite = true, -- Define o minério como infinito
-    minimum = 100, -- Quantidade mínima disponível no minério
-    normal = 1500, -- Quantidade normal disponível no minério
+    minimum = 100,   -- Quantidade mínima disponível no minério
+    normal = 1500,   -- Quantidade normal disponível no minério
     minable =
     {
-      hardness = 0.9, -- Dureza do minério (ajuste conforme desejado)
-      mining_time = 2, -- Tempo de mineração (ajuste conforme desejado)
+      hardness = 0.9,                           -- Dureza do minério (ajuste conforme desejado)
+      mining_time = 2,                          -- Tempo de mineração (ajuste conforme desejado)
       result = "charged-certus-quartz-crystal", -- Item que será produzido ao minerar o minério
     },
-    collision_box = {{ -0.1, -0.1}, {0.1, 0.1}},
-    selection_box = {{ -0.5, -0.5}, {0.5, 0.5}},
+    collision_box = { { -0.1, -0.1 }, { 0.1, 0.1 } },
+    selection_box = { { -0.5, -0.5 }, { 0.5, 0.5 } },
     autoplace =
     {
       order = "d[ore]-a[charged-certus-quartz-crystal-resource]",
@@ -40,18 +40,23 @@ data:extend({
         },
       }
     },
-    stage_counts = {1000, 600, 400, 200, 100, 50, 20, 1},
+    stage_counts = { 1000, 600, 400, 200, 100, 50, 20, 1 },
     stages =
     {
       sheet =
       {
-        filename = "__base__/graphics/icons/iron-ore.png", -- Adicione a imagem do minério
-        priority = "extra-high",
-        size = 128,
-        width = 128, -- Largura do sprite (128 pixels)
-        height = 128, -- Altura do sprite (128 pixels)
-        shift = {0, 0},
         frame_count = 8,
+        filename = "__base__/graphics/entity/iron-ore/iron-ore.png",
+        hr_version = {
+          frame_count = 8,
+          filename = "__base__/graphics/entity/iron-ore/hr-iron-ore.png",
+          scale = 0.5,
+          priority = "extra-high",
+          size = 128,
+          variation_count = 8
+        },
+        priority = "extra-high",
+        size = 64,
         variation_count = 8
       }
     },
