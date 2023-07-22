87cc5d790f53f58a760becffda03f482a11dec88
LUIS DAS ARTIMANHAS <luisaugustodesouza785@gmail.com>
2023-07-22 10:45:05 -0300

    updated

--- data/blocos/inscriber.lua
+++ data/blocos/inscriber.lua
@@ -1,95 +1,45 @@
-data:extend({
+local path_main = "__Applied-Energistics-2__/graficos/Blocos/"
 
+data:extend({
+{
+    type = "assembling-machine",
     name = "inscriber",
-    category = "entity",
-    corpse = "assembling-machine-1-remnants",
-    vehicle_impact_sound = {
+    icon = path_main .. "inscriber.png",
+    icon_size = 128,
+    flags = { "placeable-neutral", "player-creation" },
+    minable = { mining_time = 1, result = "inscriber" },
+    max_health = 200,
+    corpse = "big-remnants",
+    dying_explosion = "medium-explosion",
+    resistances = {
         {
-        filename = "__base__/sound/car-metal-impact-2.ogg",
-        volume = 0.5
-        },
-        {
-        filename = "__base__/sound/car-metal-impact-3.ogg",
-        volume = 0.5
-        },
-        {
-        filename = "__base__/sound/car-metal-impact-4.ogg",
-        volume = 0.5
-        },
-        {
-        filename = "__base__/sound/car-metal-impact-5.ogg",
-        volume = 0.5
-        },
-        {
-        filename = "__base__/sound/car-metal-impact-6.ogg",
-        volume = 0.5
+            type = "fire",
+            percent = 70
         }
     },
-    crafting_categories = "advanced-crafting",
-    crafting_speed = 0.5,
-    icon_size = 64,
-    collision_box = {
-        {-1.2,-1.2},
-        {1.2,1.2}
+    collision_box = { { -1.1, -1.1 }, { 1.1, 1.1 } },
+    selection_box = { { -1.2, -1.2 }, { 1.2, 1.2 } },
+    animation = {
+        layers = {
+            {
+                filename = path_main .. "inscriber.png",
+                priority = "extra-high",
+                width = 128,
+                height = 128,
+                frame_count = 1,
+                shift = { 0, 0 },
+            },
+        },
     },
-    energy_usage = "75kW",
-    selection_box = {
-    {-1.5,-1.5},
-    {1.5,1.5}
-    },
-    damaged_trigger_effect = {
-        entity_name = "spark-explosion",
-        type = "create-entity",
-        damage_type_filters = "fire",
-        offsets = {
-            {0,1},
-            offset_deviation = {
-                {-0.5,-0.5},
-                {0.5,0.5}
-            }
-        },
-        icon = "__base__/graphics/icons/assembling-machine-1.png",
-        working_sound = {
-            sound = {
-            filename = "__base__/sound/assembling-machine-t1-1.ogg",
-            volume = 0.5
-        },
-        fade_in_ticks = 4,
-        audible_distance_modifier = 0.5,
-        fade_out_ticks = 20
-        },
-        energy_source = {
-            emissions_per_minute = 4,
-            type = "electric",
-            usage_priority = "secondary-input"
-        },
-        type = "inscriber",
-        fast_replaceable_group = "inscriber",
-        icon_mipmaps = 4,
-        minable = {
-        mining_time = 0.2,
-        result = "inscriber"
-        },
-        alert_icon_shift = {-0.09375,-0.375},
-        flags = {
-        "placeable-neutral",
-        "placeable-player",
-        "player-creation",
-        },
-        name = "inscriber",
-        resistances = {
-        percent = 70,
-        type = "fire"
-        },
-        max_health = 300,
-        close_sound = {
-            filename = "__base__/sound/machine-close.ogg",
-            volume = 0.5
-        },
-        dying_explosion = "assembling-machine-1-explosion",
-        open_sound = {
-            filename = "__base__/sound/machine-open.ogg",
-            volume = 0.5
-        }
+    crafting_categories = { "crafting" },
+    crafting_speed = 1.5,                                                  -- O valor pode ser ajustado conforme necessário.
+    energy_usage = "300kW",                                                -- O valor pode ser ajustado conforme necessário.
+    ingredient_count = 2,-- Permite 2 ingredientes de entrada.
+    allowed_effects = { "consumption", "speed", "productivity", "pollution" }, -- Permite que a máquina beneficie-se de módulos que proporcionem esses efeitos.
+    module_specification = {
+        module_slots = 3,                                                  -- A máquina pode suportar até 3 módulos.
+        module_info_icon_shift = { 0, 0.8 },                               -- Posição do ícone de informações do módulo.
+        module_info_multi_row_initial_height_modifier = -0.3,              -- Modifica a posição vertical do ícone de informações do módulo.
     }
+},
 })
