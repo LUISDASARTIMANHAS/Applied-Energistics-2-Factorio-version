e94c3c00187a1297d9f005f25b0b1c231c8813b4
LUIS DAS ARTIMANHAS <luisaugustodesouza785@gmail.com>
2023-07-25 08:51:20 -0300

    updated

--- control/charged-certus-quartz-crystal.lua
+++ control/charged-certus-quartz-crystal.lua
@@ -8,18 +8,21 @@ data:extend({
     icon_size = 64,
     icon_mipmaps = 4,
     flags = { "placeable-neutral" },
+    category = "basic-solid",
     order = "a-b-a",
-    tree_removal_probability = 0.7,
+    tree_removal_probability = 1,
     tree_removal_max_distance = 32 * 32,
     infinite = true, -- Define o minério como infinito
-    minimum = 50,    -- Quantidade mínima disponível no minério
-    normal = 500,   -- Quantidade normal disponível no minério
+    minimum = 100,   -- Quantidade mínima disponível no minério
+    normal = 1000,   -- Quantidade normal disponível no minério
     minable =
     {
-      hardness = 0.7,-- Dureza do minério (ajuste conforme desejado)
+      hardness = 0.7,                           -- Dureza do minério (ajuste conforme desejado)
       mining_particle = "stone-particle",
-      mining_time = 2, -- Tempo de mineração (ajuste conforme desejado)
+      mining_time = 2,                          -- Tempo de mineração (ajuste conforme desejado)
       result = "charged-certus-quartz-crystal", -- Item que será produzido ao minerar o minério
+      amount_min = 1,
+      amount_max = 5,
       result_count = 1,
     },
     collision_box = { { -0.1, -0.1 }, { 0.1, 0.1 } },
@@ -27,27 +30,40 @@ data:extend({
     autoplace = {
       order = "d[ore]-a[certus-quartz-crystal-resource]",
       control = "certus-quartz-crystal-resource",
-      size_control_multiplier = 0.05,--Multiplicador do tamanho das manchas. Afeta o tamanho das áreas onde o recurso é gerado.
+      size_control_multiplier = 0.1,           --Multiplicador do tamanho das manchas. Afeta o tamanho das áreas onde o recurso é gerado.
       sharpness = 1,
-      base_density = 4,--Densidade base do recurso.
-      base_spots_per_km2 = 0.5,--Número base de manchas por quilômetro quadrado.
-      richness_multiplier = 600,-- Multiplicador da riqueza do recurso.quantidade de recurso presente em cada mancha.
-      richness_base = 500,-- Quantidade base de riqueza do recurso por mancha.
-      regular_rq_factor_multiplier = 1, --Multiplicador do fator de solicitação regular.
+      base_density = 4,                        --Densidade base do recurso.
+      richness_multiplier = 1500,              -- Multiplicador da riqueza do recurso.quantidade de recurso presente em cada mancha.
+      richness_base = 300,                     -- Quantidade base de riqueza do recurso por mancha.
+      regular_rq_factor_multiplier = 1,        --Multiplicador do fator de solicitação regular.
       starting_rq_factor_multiplier = 1.5,
-      candidate_spot_count = 22,--Número de manchas candidatas para serem geradas.
-      random_spot_size_minimum = 2,--Tamanho mínimo aleatório das manchas.
-      random_spot_size_maximum = 4,--Tamanho máximo aleatório das manchas.
+      candidate_spot_count = 22,               --Número de manchas candidatas para serem geradas.
+      random_spot_size_minimum = 2,            --Tamanho mínimo aleatório das manchas.
+      random_spot_size_maximum = 4,            --Tamanho máximo aleatório das manchas.
       has_starting_area_placement = false,
       starting_area_size = 600 * 0.0068359375, -- Tamanho da área de geração inicial
-      starting_area_amount = 1000,         -- Quantidade de recurso na área inicial
-      resource_patch_search_radius = 12,   -- Raio de busca do depósito de água
-      water_patch_search_radius = 24,      -- Raio de busca da área de água
-      water_appearance_probability = 0.05, -- Probabilidade de encontrar água (0.05 = 5%)
-      water_richness_factor = 1.3,         -- Fator de riqueza da água (1.3 = 30% mais rico)
-      water_richness_base = 500,           -- Riqueza base da água
+      starting_area_amount = 1000,             -- Quantidade de recurso na área inicial
+      resource_patch_search_radius = 12,       -- Raio de busca do depósito de água
+      water_patch_search_radius = 24,          -- Raio de busca da área de água
+      water_appearance_probability = 0.05,     -- Probabilidade de encontrar água (0.05 = 5%)
+      water_richness_factor = 1.3,             -- Fator de riqueza da água (1.3 = 30% mais rico)
+      water_richness_base = 500,               -- Riqueza base da água
+      peaks = {
+        {
+          influence = 0.2,
+          starting_area_weight_optimal = 0,
+          starting_area_weight_range = 0,
+          starting_area_weight_max_range = 2,
+        },
+        {
+          influence = 0.3,
+          noise_layer = "mineral",
+          noise_octaves_difference = -1.5,
+          noise_persistence = 0.3,
+        },
+      },
     },
-    stage_counts = { 15000, 9500, 5500, 2900, 1300, 400, 150, 80 },
+    stage_counts = { 15000, 9500, 5500, 1000, 400, 100, 50, 1 },
     stages = {
       sheet = {
         frame_count = 8,
@@ -82,7 +98,7 @@ data:extend({
   {
     type = "sprite",
     name = "certus-crystal-icon",
-    filename = path_main .. "graficos/items/charged_certus_quartz_crystal.png",-- Caminho correto do arquivo de sprite
+    filename = path_main .. "graficos/items/charged_certus_quartz_crystal.png", -- Caminho correto do arquivo de sprite
     priority = "extra-high",
     width = 64,
     height = 64,
