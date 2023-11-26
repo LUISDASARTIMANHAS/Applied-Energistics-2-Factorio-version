c5c37d3cad382584df7cbada93db3a3d80519d12
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2023-11-26 12:00:48 -0300

    updated

--- control/functions.lua
+++ control/functions.lua
@@ -4,6 +4,6 @@ function AdicionarItemNaMochilaDoJogador(player, itemName, quantidade)
     if player and player.valid then
         -- Adiciona os itens à mochila do jogador
         player.insert{name = itemName, count = quantidade}
-        game.print("AE2: Itens adicionados à mochila do jogador!")
+        game.print("AE2: Itens adicionados à mochila do jogador: " .. itemName .. ", " .. quantidade)
     end
 end
