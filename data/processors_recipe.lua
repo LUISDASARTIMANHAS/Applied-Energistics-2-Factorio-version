0414cf88c232df9d2b36f460a20518124a30e2bf
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2024-09-14 15:49:29 -0300

    updated

--- data/processors_recipe.lua
+++ data/processors_recipe.lua
@@ -1,3 +1,4 @@
+local path_main = "__Applied-Energistics-2__/"
 -- category = "advanced-crafting" maquinas de montagem tier 2 e 3
 -- category = "basic-crafting" maquinas de montagem tier 1
 -- category = "crafting" feito a mão
@@ -5,110 +6,182 @@
 -- category = "centrifuging" feito na centrifuge
 -- category = "inscriber"
 
--- PROCESSOR PRECESS REPLICATION
-data:extend({ -- receita
-{
-    type = "recipe",
-    name = "silicon-press-recipe",
-    category = "inscriber",
-    enabled = false,
-    ingredients = {{"silicon-press", 1}, {"iron-block", 1}},
-    energy_required = 60,
-    result_count = 2,
-    result = "silicon-press"
-}, {
-    type = "recipe",
-    name = "logic-processor-press-recipe",
-    category = "inscriber",
-    enabled = false,
-    ingredients = {{"logic-processor-press", 1}, {"iron-block", 1}},
-    energy_required = 60,
-    result_count = 2,
-    result = "logic-processor-press"
-}, {
-    type = "recipe",
-    name = "calculation-processor-press-recipe",
-    category = "inscriber",
-    enabled = false,
-    ingredients = {{"calculation-processor-press", 1}, {"iron-block", 1}},
-    energy_required = 60,
-    result_count = 2,
-    result = "calculation-processor-press"
-}, {
-    type = "recipe",
-    name = "engineering-processor-press-recipe",
-    category = "inscriber",
-    enabled = false,
-    ingredients = {{"engineering-processor-press", 1}, {"iron-block", 1}},
-    energy_required = 60,
-    result_count = 2,
-    result = "engineering-processor-press"
-}})
-
 -- PRINTED CIRCUITS
-data:extend({ -- receita
-{
-    type = "recipe",
-    name = "silicon-press-recipe",
-    category = "inscriber",
-    enabled = false,
-    ingredients = {{"silicon-press", 1}, {"iron-block", 1}},
-    energy_required = 60,
-    result_count = 2,
-    result = "silicon-press"
-}, {
-    type = "recipe",
-    name = "logic-processor-press-recipe",
-    category = "inscriber",
-    enabled = false,
-    ingredients = {{"logic-processor-press", 1}, {"iron-block", 1}},
-    energy_required = 60,
-    result_count = 2,
-    result = "logic-processor-press"
-}, {
-    type = "recipe",
-    name = "calculation-processor-press-recipe",
-    category = "inscriber",
-    enabled = false,
-    ingredients = {{"calculation-processor-press", 1}, {"iron-block", 1}},
-    energy_required = 60,
-    result_count = 2,
-    result = "calculation-processor-press"
-}, {
-    type = "recipe",
-    name = "engineering-processor-press-recipe",
-    category = "inscriber",
-    enabled = false,
-    ingredients = {{"engineering-processor-press", 1}, {"iron-block", 1}},
-    energy_required = 60,
-    result_count = 2,
-    result = "engineering-processor-press"
-}})
+data:extend(
+    {
+        -- definindo o item
+        {
+            type = "item",
+            name = "printed-silicon",
+            icon = path_main .. "graficos/itens/printed_silicon.png",
+            icon_size = 128,
+            subgroup = "data-terminal",
+            order = "AE2-component[printed-silicon]",
+            stack_size = 1
+        }, -- receita
+        {
+            type = "recipe",
+            name = "printed-silicon-recipe",
+            category = "inscriber",
+            enabled = true,
+            energy_required = 20,
+            ingredients = {
+                {"silicon", 1},
+                {"silicon-press", 1}
+            },
+            result = {
+                {"printed-silicon", 1},
+                {"silicon-press", 1}
+            }
+        },
+        -- definindo o item
+        {
+            type = "item",
+            name = "printed-logic-processor",
+            icon = path_main .. "graficos/itens/printed_logic_processor.png",
+            icon_size = 128,
+            subgroup = "data-terminal",
+            order = "AE2-component[printed-logic-processor]",
+            stack_size = 1
+        }, -- receita
+        {
+            type = "recipe",
+            name = "printed-logic-processor-recipe",
+            category = "inscriber",
+            enabled = true,
+            energy_required = 20,
+            ingredients = {
+                {"advanced-circuit", 1},
+                {"logic-processor-press", 1}
+            },
+            result = {
+                {"printed-logic-processor", 1},
+                {"logic-processor-press", 1}
+            }
+        },
+        {
+            type = "item",
+            name = "printed-calculation-processor",
+            icon = path_main .. "graficos/itens/printed_calculation_processor.png",
+            icon_size = 128,
+            subgroup = "data-terminal",
+            order = "AE2-component[printed-calculation-processor]",
+            stack_size = 1
+        }, -- receita
+        {
+            type = "recipe",
+            name = "printed-calculation-processor-recipe",
+            category = "inscriber",
+            enabled = true,
+            energy_required = 20,
+            ingredients = {
+                {"processing-unit", 1},
+                {"calculation-processor-press", 1}
+            },
+            result = {
+                {"printed-calculation-processor", 1},
+                {"calculation-processor-press", 1}
+            }
+        },
+        {
+            type = "item",
+            name = "printed-engineering-processor",
+            icon = path_main .. "graficos/itens/printed_engineering_processor.png",
+            icon_size = 128,
+            subgroup = "data-terminal",
+            order = "AE2-component[printed-engineering-processor]",
+            stack_size = 1
+        }, -- receita
+        {
+            type = "recipe",
+            name = "printed-engineering-processor-recipe",
+            category = "inscriber",
+            enabled = true,
+            energy_required = 20,
+            ingredients = {
+                {"certus-quartz-crystal", 1},
+                {"engineering-processor-press", 1}
+            },
+            result = {
+                {"printed-engineering-processor", 1},
+                {"engineering-processor-press", 1}
+            }
+        }
+    }
+)
+
 
 -- PROCESSORS
-data:extend({ -- receita
-{
-    type = "recipe",
-    name = "logic-processor-recipe",
-    category = "inscriber",
-    enabled = false,
-    ingredients = {{"logic-processor-press", 1}, {"silicon-press", 1}, {"copper-cable", 1}},
-    energy_required = 60,
-    result = "logic-processor"
-}, {
-    type = "recipe",
-    name = "calculation-processor-recipe",
-    category = "inscriber",
-    enabled = false,
-    ingredients = {{"calculation-processor-press", 1}, {"silicon-press", 1}, {"copper-cable", 1}},
-    energy_required = 60,
-    result = "calculation-processor"
-}, {
-    type = "recipe",
-    name = "engineering-processor-recipe",
-    category = "inscriber",
-    enabled = false,
-    ingredients = {{"engineering-processor-press", 1}, {"silicon-press", 1}, {"copper-cable", 1}},
-    energy_required = 60,
-    result = "engineering-processor"
-}})
+data:extend(
+    {
+        -- definindo o item
+        {
+            type = "item",
+            name = "logic-processor",
+            icon = path_main .. "graficos/itens/logic_processor.png",
+            icon_size = 128,
+            subgroup = "data-terminal",
+            order = "AE2-processing[logic-processor]",
+            stack_size = 1
+        }, -- receita
+        {
+            type = "recipe",
+            name = "logic-processor-recipe",
+            category = "inscriber",
+            enabled = true,
+            energy_required = 60,
+            ingredients = {
+                {"printed-logic-processor", 1},
+                {"copper-cable", 20},
+                {"printed-silicon", 1}
+            },
+            result = "logic-processor"
+        },
+        -- definindo o item
+        {
+            type = "item",
+            name = "calculation-processor",
+            icon = path_main .. "graficos/itens/calculation_processor.png",
+            icon_size = 128,
+            subgroup = "data-terminal",
+            order = "AE2-processing[calculation-processor]",
+            stack_size = 1
+        }, -- receita
+        {
+            type = "recipe",
+            name = "calculation-processor-recipe",
+            category = "inscriber",
+            enabled = true,
+            energy_required = 60,
+            ingredients = {
+                {"printed-calculation-processor", 1},
+                {"copper-cable", 20},
+                {"printed-silicon", 1}
+            },
+            result = "calculation-processor"
+        },
+        -- definindo o item
+        {
+            type = "item",
+            name = "engineering-processor",
+            icon = path_main .. "graficos/itens/engineering_processor.png",
+            icon_size = 128,
+            subgroup = "data-terminal",
+            order = "AE2-processing[engineering-processor]",
+            stack_size = 1
+        }, -- receita
+        {
+            type = "recipe",
+            name = "engineering-processor-recipe",
+            category = "inscriber",
+            enabled = true,
+            energy_required = 60,
+            ingredients = {
+                {"printed-engineering-processor", 1},
+                {"copper-cable", 20},
+                {"printed-silicon", 1}
+            },
+            result = "engineering-processor"
+        },
+    }
+)
--- data/recursos_brutos.lua
+++ data/recursos_brutos.lua
@@ -1,204 +1,384 @@
 local path_main = "__Applied-Energistics-2__/"
 
-    -- category = "advanced-crafting" maquinas de montagem tier 2 e 3
-    -- category = "basic-crafting" maquinas de montagem tier 1
-    -- category = "crafting" feito a mão
-    -- category = "smelting" feito em fornalhas
-    -- category = "centrifuging" feito na centrifuge
-    -- category = "inscriber"
+-- category = "advanced-crafting" maquinas de montagem tier 2 e 3
+-- category = "basic-crafting" maquinas de montagem tier 1
+-- category = "crafting" feito a mão
+-- category = "smelting" feito em fornalhas
+-- category = "centrifuging" feito na centrifuge
+-- category = "inscriber"
 
-data:extend({
--- definindo o item
-{
-    type = "item",
-    name = "charged-certus-quartz-crystal",
-    icon = path_main.. "graficos/items/charged_certus_quartz_crystal.png",
-    icon_size = 128,
-    subgroup = "resources-generated",
-    order = "AE2-resources-generated[charged-certus-quartz-crystal]",
-    stack_size = 64,
-}
-})
-data:extend({
--- definindo o item
+data:extend(
     {
-    type = "item",
-    name = "fluix-crystal",
-    icon = path_main.. "graficos/items/fluix_crystal.png",
-    icon_size = 128,
-    subgroup = "resources-generated",
-    order = "AE2-resources-generated[fluix-crystal]",
-    stack_size = 64,
-    },
-    -- receita
+        -- definindo o item
+        {
+            type = "item",
+            name = "quartz",
+            icon = path_main .. "graficos/itens/quartz.png",
+            icon_size = 128,
+            subgroup = "data-terminal",
+            order = "resources-generated[quartz]",
+            stack_size = 64
+        }, -- receita
+        {
+            type = "recipe",
+            name = "quartz-recipe",
+            category = "charger",
+            enabled = true,
+            energy_required = 20,
+            ingredients = {{"stone", 20}},
+            result = "quartz"
+        }
+    }
+)
+
+data:extend(
     {
-    type = "recipe",
-    name = "fluix-crystal-recipe",
-    category = "advanced-crafting",
-    enabled = true,
-    always_show_madein = true,
-    ingredients = {
-        {"charged-certus-quartz-crystal",1}
-    },
-    energy_required = 60,
-    result = "fluix-crystal",
-    },
-})
+        -- definindo o item
+        {
+            type = "item",
+            name = "charged-certus-quartz-crystal",
+            icon = path_main .. "graficos/itens/charged_certus_quartz_crystal.png",
+            icon_size = 128,
+            subgroup = "data-terminal",
+            order = "resources-generated[charged-certus-quartz-crystal]",
+            stack_size = 64
+        }, -- receita
+        {
+            type = "recipe",
+            name = "charged-certus-quartz-crystal-recipe",
+            category = "charger",
+            enabled = true,
+            energy_required = 120,
+            ingredients = {{"certus-quartz-crystal", 1}},
+            result = "charged-certus-quartz-crystal"
+        },
+        {
+            type = "recipe",
+            name = "charged-certus-quartz-crystal-recipe",
+            category = "advanced-crafting",
+            enabled = true,
+            energy_required = 45,
+            ingredients = {
+                {"certus-quartz-dust", 1},
+                {
+                    type = "fluid",
+                    name = "water",
+                    amount = 100
+                }
+            },
+            result = "charged-certus-quartz-crystal"
+        }
+    }
+)
 
-data:extend({
--- definindo o item
-{
-    type = "item",
-    name = "silicon-press",
-    icon = path_main.. "graficos/items/silicon_press.png",
-    icon_size = 128,
-    subgroup = "processing",
-    order = "AE2-processing[silicon-press]",
-    stack_size = 1,
-},
--- receita
-{
-    type = "recipe",
-    name = "silicon-press-recipe",
-    category = "inscriber",
-    enabled = true,
-    always_show_madein = true,
-    ingredients = {
-        {"iron-block", 1},
-        {"fluix-crystal",2}
-    },
-    energy_required = 120,
-    result_count = 2,
-    result = "silicon-press",
-},
-})
-data:extend({
--- definindo o item
-{
-    type = "item",
-    name = "engineering-processor-press",
-    icon = path_main.. "graficos/items/engineering_processor_press.png",
-    icon_size = 128,
-    subgroup = "processing",
-    order = "AE2-processing[engineering-processor-press]",
-    stack_size = 64,
-},
--- receita
-{
-    type = "recipe",
-    name = "engineering-processor-press-recipe",
-    category = "inscriber",
-    enabled = true,
-    ingredients = {
-        { "engineering-processor-press", 1 },
-        { "iron-block",                  9 }
-    },
-    energy_required = 120,
-    result_count = 2,
-    result = "engineering-processor-press",
-},
-})
-data:extend({
--- definindo o item
-{
-    type = "item",
-    name = "logic-processor-press",
-    icon = path_main.. "graficos/items/logic_processor_press.png",
-    icon_size = 128,
-    subgroup = "processing",
-    order = "AE2-processing[logic-processor-press]",
-    stack_size = 1,
-},
--- receita
-{
-    type = "recipe",
-    name = "logic-processor-press-recipe",
-    category = "inscriber",
-    enabled = true,
-    ingredients = {
-        { "logic-processor-press", 1 },
-        { "iron-block",            9 }
-    },
-    energy_required = 120,
-    result_count = 2,
-    result = "logic-processor-press",
-},
-})
-data:extend({
-    -- definindo o item
+data:extend(
     {
-        type = "item",
-        name = "calculation-processor-press",
-        icon = path_main.. "graficos/items/calculation_processor_press.png",
-        icon_size = 128,
-        subgroup = "processing",
-        order = "AE2-processing[calculation-processor-press]",
-        stack_size = 1,
-    },
-    -- receita
-{
-    type = "recipe",
-    name = "calculation-processor-press-recipe",
-    category = "inscriber",
-    enabled = true,
-    ingredients = {
-        { "calculation-processor-press", 1 },
-        { "iron-block",                  9 }
-    },
-    energy_required = 120,
-    result_count = 2,
-    result = "calculation-processor-press",
-},
-    })
+        -- definindo o item
+        {
+            type = "item",
+            name = "certus-quartz-dust",
+            icon = path_main .. "graficos/itens/certus_quartz_dust.png",
+            icon_size = 128,
+            subgroup = "data-terminal",
+            order = "resources-generated[certus-quartz-dust]",
+            stack_size = 64
+        }, -- receita
+        {
+            type = "recipe",
+            name = "certus-quartz-dust-recipe",
+            category = "inscriber",
+            enabled = true,
+            ingredients = {{"certus-quartz-crystal", 1}},
+            result = "certus-quartz-dust"
+        }
+    }
+)
 
-data:extend({
--- definindo o item
-{
-    type = "item",
-    name = "iron-block",
-    icon = path_main.. "graficos/items/iron_block.png",
-    icon_size = 128,
-    subgroup = "component",
-    order = "AE2-component[iron-block]",
-    stack_size = 64,
-},
--- receita
-{
-    type = "recipe",
-    name = "iron-block-recipe",
-    category = "basic-crafting",
-    enabled = true,
-    always_show_madein = true,
-    ingredients = {
-        {"iron-plate",9}
-    },
-    energy_required = 9,
-    result = "iron-block"
-},
-})
+data:extend(
+    {
+        -- definindo o item
+        {
+            type = "item",
+            name = "silicon",
+            icon = path_main .. "graficos/itens/silicon.png",
+            icon_size = 128,
+            subgroup = "data-terminal",
+            order = "resources-generated[silicon]",
+            stack_size = 64
+        }, -- receita
+        {
+            type = "recipe",
+            name = "silicon-recipe",
+            category = "smelting",
+            enabled = true,
+            energy_required = 20,
+            ingredients = {{"certus-quartz-dust", 1}},
+            result = "silicon"
+        }
+    }
+)
 
-data:extend({
--- definindo o item
-{
-    type = "item",
-    name = "compressed-iron-block",
-    icon = path_main.. "graficos/items/compressed_iron_block.png",
-    icon_size = 128,
-    subgroup = "component",
-    order = "AE2-component[compressed-iron-block]",
-    stack_size = 64,
-},
--- receita
-{
-    type = "recipe",
-    name = "compressed-iron-block-recipe",
-    category = "advanced-crafting",
-    enabled = true,
-    always_show_madein = true,
-    ingredients = {
-        {"iron-block",9}
-    },
-    energy_required = 12,
-    result = "compressed-iron-block"
-},
-})
+data:extend(
+    {
+        -- definindo o item
+        {
+            type = "item",
+            name = "fluix-crystal",
+            icon = path_main .. "graficos/itens/fluix_crystal.png",
+            icon_size = 128,
+            subgroup = "resources-generated",
+            order = "AE2-resources-generated[fluix-crystal]",
+            stack_size = 64
+        }, -- receita
+        {
+            type = "recipe",
+            name = "fluix-crystal-recipe",
+            category = "advanced-crafting",
+            enabled = true,
+            always_show_madein = true,
+            ingredients = {
+                {
+                    {"charged-certus-quartz-crystal", 1},
+                    {"copper-cable", 1},
+                    {"quartz", 1},
+                    {
+                        type = "fluid",
+                        name = "water",
+                        amount = 100
+                    }
+                }
+            },
+            energy_required = 60,
+            result = "fluix-crystal"
+        },
+        {
+            type = "recipe",
+            name = "fluix-crystal-recipe",
+            category = "advanced-crafting",
+            enabled = true,
+            always_show_madein = true,
+            ingredients = {
+                {
+                    {"fluix-dust", 1},
+                    {"charged-certus-quartz-crystal", 1},
+                    {
+                        type = "fluid",
+                        name = "water",
+                        amount = 100
+                    }
+                }
+            },
+            energy_required = 60,
+            result = "fluix-crystal"
+        }
+    }
+)
+
+data:extend(
+    {
+        -- definindo o item
+        {
+            type = "item",
+            name = "fluix-dust",
+            icon = path_main .. "graficos/itens/fluix_dust.png",
+            icon_size = 128,
+            subgroup = "data-terminal",
+            order = "resources-generated[certus-quartz-dust]",
+            stack_size = 64
+        }, -- receita
+        {
+            type = "recipe",
+            name = "fluix-dust-recipe",
+            category = "inscriber",
+            enabled = true,
+            ingredients = {{"fluix-crystal", 1}},
+            result = "fluix-dust"
+        }
+    }
+)
+
+data:extend(
+    {
+        -- definindo o item
+        {
+            type = "item",
+            name = "fluix-block",
+            icon = path_main .. "graficos/blocos/fluix_block.png",
+            icon_size = 128,
+            subgroup = "resources-generated",
+            order = "AE2-resources-generated[fluix-block]",
+            stack_size = 64
+        }, -- receita
+        {
+            type = "recipe",
+            name = "fluix-crystal-recipe",
+            category = "crafting",
+            enabled = true,
+            always_show_madein = true,
+            ingredients = {{"fluix-crystal", 4}},
+            energy_required = 10,
+            result = "fluix-block"
+        }
+    }
+)
+
+-- ========== PROCESSOR PRECESS REPLICATION ==========
+data:extend(
+    {
+        -- definindo o item
+        {
+            type = "item",
+            name = "silicon-press",
+            icon = path_main .. "graficos/itens/silicon_press.png",
+            icon_size = 128,
+            subgroup = "processing",
+            order = "AE2-processing[silicon-press]",
+            stack_size = 1
+        }, -- receita
+        {
+            type = "recipe",
+            name = "silicon-press-recipe",
+            category = "inscriber",
+            enabled = true,
+            always_show_madein = true,
+            ingredients = {{"iron-block", 1}, {"silicon-press", 1}},
+            energy_required = 120,
+            result_count = 2,
+            result = "silicon-press"
+        },
+        -- definindo o item
+        {
+            type = "item",
+            name = "engineering-processor-press",
+            icon = path_main .. "graficos/itens/engineering_processor_press.png",
+            icon_size = 128,
+            subgroup = "processing",
+            order = "AE2-processing[engineering-processor-press]",
+            stack_size = 64
+        }, -- receita
+        {
+            type = "recipe",
+            name = "engineering-processor-press-recipe",
+            category = "inscriber",
+            enabled = true,
+            ingredients = {{"engineering-processor-press", 1}, {"iron-block", 1}},
+            energy_required = 120,
+            result_count = 2,
+            result = "engineering-processor-press"
+        },
+        -- definindo o item
+        {
+            type = "item",
+            name = "logic-processor-press",
+            icon = path_main .. "graficos/itens/logic_processor_press.png",
+            icon_size = 128,
+            subgroup = "processing",
+            order = "AE2-processing[logic-processor-press]",
+            stack_size = 1
+        }, -- receita
+        {
+            type = "recipe",
+            name = "logic-processor-press-recipe",
+            category = "inscriber",
+            enabled = true,
+            ingredients = {{"logic-processor-press", 1}, {"iron-block", 1}},
+            energy_required = 120,
+            result_count = 2,
+            result = "logic-processor-press"
+        },
+        -- definindo o item
+        {
+            type = "item",
+            name = "calculation-processor-press",
+            icon = path_main .. "graficos/itens/calculation_processor_press.png",
+            icon_size = 128,
+            subgroup = "processing",
+            order = "AE2-processing[calculation-processor-press]",
+            stack_size = 1
+        }, -- receita
+        {
+            type = "recipe",
+            name = "calculation-processor-press-recipe",
+            category = "inscriber",
+            enabled = true,
+            ingredients = {{"calculation-processor-press", 1}, {"iron-block", 1}},
+            energy_required = 120,
+            result_count = 2,
+            result = "calculation-processor-press"
+        }
+    }
+)
+
+-- ============blocos==========
+data:extend(
+    {
+        -- definindo o item
+        {
+            type = "item",
+            name = "certus-quartz-block",
+            icon = path_main .. "graficos/blocos/quartz_block.png",
+            icon_size = 128,
+            subgroup = "data-terminal",
+            order = "resources-generated[certus-quartz-block]",
+            stack_size = 1
+        }, -- receita
+        {
+            type = "recipe",
+            name = "certus-quartz-block-recipe",
+            category = "crafting",
+            enabled = true,
+            ingredients = {{"certus-quartz-cristal", 4}},
+            result = "certus-quartz-block"
+        }
+    }
+)
+
+data:extend(
+    {
+        -- definindo o item
+        {
+            type = "item",
+            name = "iron-block",
+            icon = path_main .. "graficos/blocos/iron_block.png",
+            icon_size = 128,
+            subgroup = "component",
+            order = "AE2-component[iron-block]",
+            stack_size = 64
+        }, -- receita
+        {
+            type = "recipe",
+            name = "iron-block-recipe",
+            category = "basic-crafting",
+            enabled = true,
+            always_show_madein = true,
+            ingredients = {{"iron-plate", 9}},
+            energy_required = 9,
+            result = "iron-block"
+        }
+    }
+)
+
+data:extend(
+    {
+        -- definindo o item
+        {
+            type = "item",
+            name = "compressed-iron-block",
+            icon = path_main .. "graficos/blocos/compressed_iron_block.png",
+            icon_size = 128,
+            subgroup = "component",
+            order = "AE2-component[compressed-iron-block]",
+            stack_size = 64
+        }, -- receita
+        {
+            type = "recipe",
+            name = "compressed-iron-block-recipe",
+            category = "advanced-crafting",
+            enabled = true,
+            always_show_madein = true,
+            ingredients = {{"iron-block", 9}},
+            energy_required = 12,
+            result = "compressed-iron-block"
+        }
+    }
+)
